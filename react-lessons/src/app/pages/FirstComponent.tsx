87d9121e3ac2a7eb66840b49296a1eaae1a97980
SrinivasanSainsburys <srinivasan.sampath@sainsburys.co.uk>
2025-07-23 19:51:14 +0530

    Added two new components and passing the value from parent to child in typescript

---
+++ react-lessons/src/app/pages/FirstComponent.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import React, { useState } from 'react'
+import SecondComponent from './SecondComponent'
+
+interface FirstComponentProps {
+    userName: string,
+}
+
+const FirstComponent:React.FC<FirstComponentProps> = ({userName}) => {
+
+  const [description, setDescription] = useState('')
+ 
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value)
+  }
+
+  return (
+    <div>
+        <h2>{`Hi... ${userName}`}</h2>
+        Your description : <input className='border-4 border-white pl-3' type='text' onChange={(event) => handleOnChange(event)} value = {description} placeholder='Enter your description'/>
+        <SecondComponent desc={description}/>
+    </div>
+  )
+}
+
+export default FirstComponent
