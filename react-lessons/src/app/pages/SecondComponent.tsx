2a7a4d2ecd67e2f41bd68b1043bd041bc8d54d81
abhinavp296 <abhinav.pandey@sainsburys.co.uk>
2025-07-25 12:06:29 +0100

    Create SecondComponent.tsx

---
+++ react-lessons/src/app/pages/SecondComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface SecondComponentProps {
+    desc: string
+}
+
+const SecondComponent: React.FC<SecondComponentProps> = ({ desc }) => {
+    return (
+        <h1>{desc}</h1>
+    )
+}
+
+export default SecondComponent
